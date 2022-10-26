c4718fabe228577c11110b4ea001901d69159287
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-10-26 14:57:48 +0300

    Добавили дополнительное занятие

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import { users } from './data/users';
+import { User } from './User/User';
+import { UserList } from './UserList/UserList';
+import { Section } from './UserList/Section/Section';
+
 export const App = () => {
   return (
-    <div>
-    
-    </div>
+    <>
+      <Section title="List of users">
+        <UserList users={users} />
+      </Section>
+      <Section>
+        <User user={users[0]} />
+      </Section>
+    </>
   );
 };
